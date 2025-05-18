86bddefc6e190670f0d7d563f3bdf541c6670c08
Antrax <fredcox@gmail.com>
2025-05-18 20:44:34 -0300

    now with gtk4-layer-shell

--- src/hyprbar/window.py
+++ src/hyprbar/window.py
@@ -1,26 +1,34 @@
-import sys
-import signal
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtGui import QGuiApplication
-from hyprbar.constants import APP_NAME
-from hyprbar.util import updateQtEnvironment
+from ctypes import CDLL
+
+CDLL("libgtk4-layer-shell.so")
+import gi
+
+gi.require_version("Gtk", "4.0")
+gi.require_version("Gtk4LayerShell", "1.0")
+from gi.repository import Gtk, Gtk4LayerShell
 
 
-class Window(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle(f"{APP_NAME}")
-        self.setObjectName(f"{APP_NAME}")
-        # Set application name and class before creating QWidget
-        QGuiApplication.setApplicationName(f"{APP_NAME}")
-        QGuiApplication.setDesktopFileName(f"{APP_NAME}")
-        QGuiApplication.setApplicationDisplayName(f"{APP_NAME}")
+class StatusBarApp(Gtk.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect("activate", self.on_activate)
+
+    def on_activate(self, app):
+        window = Gtk.ApplicationWindow(application=app)
+        label = Gtk.Label(label="GTK4 Layer Shell com Python!")
+        window.set_child(label)
+
+        # Inicialize a janela com Layer Shell
+        Gtk4LayerShell.init_for_window(window)
+        Gtk4LayerShell.auto_exclusive_zone_enable(window)
+        Gtk4LayerShell.set_margin(window, Gtk4LayerShell.Edge.TOP, 0)
+        Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.TOP, True)
+        Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.LEFT, True)
+        Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.RIGHT, True)
+
+        window.present()
 
 
 if __name__ == "__main__":
-    updateQtEnvironment()
-    app = QApplication(sys.argv)
-    window = Window()
-    window.show()
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
-    sys.exit(app.exec())
+    app = StatusBarApp(application_id="com.example.StatusBar")
+    app.run()
