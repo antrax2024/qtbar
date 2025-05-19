09f14dfc5088d41eca75dd34a449e17bf05442e3
Antrax <fredcox@gmail.com>
2025-05-18 21:48:08 -0300

    update util.py

--- src/hyprbar/constants.py
+++ src/hyprbar/constants.py
@@ -7,8 +7,4 @@ SPACES_DEFAULT = 15
 
 CONFIG_DIR = os.path.join(os.path.expanduser(path="~"), ".config", f"{APP_NAME}")
 CONFIG_FILE = os.path.join(CONFIG_DIR, "config.yaml")
-
-WINDOW_RULES = [
-    "hyprctl dispatch focuswindow class:hyprbar",
-    "hyprctl dispatch togglefloating",
-]
+STYLE_FILE = os.path.join(CONFIG_DIR, "styles.css")
