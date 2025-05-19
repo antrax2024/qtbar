7811602281736421fa666cb7392a9aed7dba482e
Antrax <fredcox@gmail.com>
2025-05-18 21:16:29 -0300

    now its ok

--- src/hyprbar/window.py
+++ src/hyprbar/window.py
@@ -1,34 +1,37 @@
+# To run this script without installing the library, set GI_TYPELIB_PATH and LD_LIBRARY_PATH to the build/src directory
+# GI_TYPELIB_PATH=build/src LD_LIBRARY_PATH=build/src python3 examples/simple-example.py
+
+# For GTK4 Layer Shell to get linked before libwayland-client we must explicitly load it before importing with gi
 from ctypes import CDLL
 
 CDLL("libgtk4-layer-shell.so")
+
 import gi  # noqa
 
 gi.require_version("Gtk", "4.0")
 gi.require_version("Gtk4LayerShell", "1.0")
-from gi.repository import Gtk, Gtk4LayerShell  # pyright: ignore # noqa
+
+from gi.repository import Gtk  # pyright: ignore #noqa
+from gi.repository import Gtk4LayerShell as LayerShell  # pyright: ignore #noqa
 
 
-class HyprBar(Gtk.Application):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        self.connect("activate", self.on_activate)
+def on_activate(app):
+    window = Gtk.Window(application=app)
+    window.set_default_size(400, 70)
 
-    def on_activate(self, app):
-        window = Gtk.ApplicationWindow(application=app)
-        label = Gtk.Label(label="GTK4 Layer Shell com Python!")
-        window.set_child(label)
+    LayerShell.init_for_window(window)
+    LayerShell.set_layer(window, LayerShell.Layer.TOP)
+    LayerShell.set_anchor(window, LayerShell.Edge.BOTTOM, True)
+    LayerShell.set_margin(window, LayerShell.Edge.BOTTOM, 20)
+    LayerShell.set_margin(window, LayerShell.Edge.TOP, 20)
+    LayerShell.auto_exclusive_zone_enable(window)
 
-        # Inicialize a janela com Layer Shell
-        Gtk4LayerShell.init_for_window(window)
-        Gtk4LayerShell.auto_exclusive_zone_enable(window)
-        Gtk4LayerShell.set_margin(window, Gtk4LayerShell.Edge.TOP, 0)
-        Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.TOP, True)
-        Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.LEFT, True)
-        Gtk4LayerShell.set_anchor(window, Gtk4LayerShell.Edge.RIGHT, True)
-
-        window.present()
+    button = Gtk.Button(label="GTK4 Layer Shell with Python")
+    button.connect("clicked", lambda x: window.close())
+    window.set_child(button)
+    window.present()
 
 
-if __name__ == "__main__":
-    app = HyprBar(application_id="com.antrax.HyprBar")
-    app.run()
+app = Gtk.Application(application_id="com.github.wmww.gtk4-layer-shell.py-example")
+app.connect("activate", on_activate)
+app.run(None)
