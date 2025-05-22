5e82ab98ed8654a097a10e95d04a0201b8c91a95
Antrax <fredcox@gmail.com>
2025-05-22 18:25:21 -0300

    i added another paradigm to config.py

--- src/hyprbar/widgets.py
+++ src/hyprbar/widgets.py
@@ -3,6 +3,7 @@ import threading
 import time
 from gi.repository import Gtk  # pyright: ignore #noqa
 from gi.repository import GLib  # pyright: ignore # noqa
+from hyprbar.util import printLog
 from hyprpy import Hyprland
 
 from hyprbar.config import ComponentConfig
@@ -13,18 +14,19 @@ currentWorkspaceID = 1
 
 
 def populateBox(box: Gtk.Box, components: List[ComponentConfig]) -> None:
+    printLog(f"Populating box => {box} with components")
     for comp in components:
         if comp.type == "workspaces":
+            printLog(f"Creating workspaces component => {comp.text}")  # pyright: ignore # noqa
             createWorkspacesComponent(box=box, workspace_text=comp.text)  # pyright: ignore # noqa
         elif comp.type == "clock":
+            printLog(f"Creating clock component => {comp.icon}")  # pyright: ignore # noqa
             createClockComponent(
                 box=box,
-                icon=comp.icon,  # pyright: ignore # noqa
-                format=comp.format,  # pyright: ignore # noqa
-                refresh=comp.refresh,  # pyright: ignore # noqa
-                css_id=comp.css_id,  # pyright: ignore # noqa
+                comp=comp,
             )
         elif comp.type == "label":
+            printLog(f"Creating label component => {comp.text}")  # pyright: ignore # noqa
             print(f"Label: {comp.text}")  # pyright: ignore # noqa
 
 
@@ -87,24 +89,20 @@ def clockThread(clockLabel: Gtk.Label, format: str, refresh: int) -> None:
         time.sleep(refresh)
 
 
-def createClockComponent(
-    box: Gtk.Box,
-    icon: str,
-    format: str,
-    css_id: str,
-    refresh: int = 1,
-):
-    iconLabel = Gtk.Label(label=f"{icon}")
-    iconLabel.set_name(f"{css_id}-icon")
+def createClockComponent(box: Gtk.Box, comp: ComponentConfig):
+    iconLabel = Gtk.Label(label=f"{comp.icon}")  # pyright: ignore # noqa
+    iconLabel.set_name(f"{comp.css_id}-icon")  # pyright: ignore # noqa
     clockLabel = Gtk.Label()
-    clockLabel.set_name(f"{css_id}-label")
+    clockLabel.set_name(f"{comp.css_id}-label")  # pyright: ignore # noqa
 
+    printLog(f"Starting thread for clock component...")  # pyright: ignore # noqa
     thread = threading.Thread(
         target=clockThread,
-        args=(clockLabel, format, refresh),
+        args=(clockLabel, comp.format, comp.refresh),  # pyright: ignore # noqa
         daemon=True,
     )
     thread.start()
+    printLog(f"Append icon and label to box...")  # pyright: ignore # noqa
     box.append(iconLabel)
     box.append(clockLabel)
 
